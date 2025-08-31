ff70a6227a508b7259ce5f1849fcd59789cb6f7a
sirasaki-konoha <rock-db@outlook.jp>
2025-08-31 12:23:32 +0900

    Implement file removal functionality with confirmation prompts and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod macros;
 mod write;
 mod stdin;
 mod size;
+mod rm;
 
 /// Visual Fire Read System vira
 #[derive(Parser)]
@@ -15,16 +16,22 @@ struct Cli {
     #[arg(short, long)]
     pub line: bool,
 
-    /// Write the following string to a file
+    /// Writes the following content to a specified file
     #[arg(short, long, value_names = &["OPTION", "CONTENT"], num_args(1..))]
     pub write: Option<Vec<String>>,
     
+    // Display specific file size
     #[arg(short, long)]
     pub size: bool,
 
+    /// Remove a specified file
     #[arg(short, long)]
     pub remove: bool,
+    /// Check before removing a file
+    #[arg(short, long)]
+    pub check: bool,
 
+    /// Creates a specified file
     #[arg(short, long)]
     pub new: bool,
 }
@@ -44,9 +51,17 @@ fn main() {
             err!("Failed to open {}: {}", file_name, e);
             exit(1);
         });
-    } else if cmd.size {
+    }
+
+    if cmd.size {
         flag = true;
         size::get_size(&file_name);
+    }
+    
+    if cmd.remove {
+        flag = true;
+        rm::remove_file(&file_name, cmd.check);
+        return;
     }
     
     if let Some(write_cmd) = cmd.write {
---
+++ src/rm.rs
@@ -0,0 +1,49 @@
+use std::{fs, path::Path};
+use crate::err;
+
+use crossterm::style::Stylize;
+
+
+
+
+pub fn remove_file<P: AsRef<Path>>(path: P, check: bool) {
+    let path = path.as_ref();
+
+    if path.is_dir() {
+        let p = inquire::Confirm::new(&format!(
+                "{} is a directory, do you want to remove it? (y/N): ",
+                path.display()
+            ))
+            .with_default(false)
+            .prompt()
+            .unwrap_or(false);
+        
+        if !p {
+            println!("{} Aborted.", ">>>".cyan().dim());
+            return;
+        }
+
+        fs::remove_dir_all(path).unwrap_or_else(|e| {
+            err!("Failed to remove {}: {}", path.display(), e);
+            return;
+        });
+    }        
+    
+    if path.is_file() {
+        if check {
+            let p = inquire::Confirm::new(&format!("Do you want to remove {}?", path.display()))
+                .with_default(false)
+                .prompt()
+                .unwrap_or(false);
+            if !p {
+                println!("{} Aborted.", ">>>".cyan().dim());
+                return;
+            }
+        }
+        fs::remove_file(path).unwrap_or_else(|e| {
+            err!("Failed to remove {}: {}", path.display(), e);
+            return;
+        });
+    }
+} 
+
